7cb28a6929bbbc8405bade6eceb878a17bea8d5c
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-10-24 18:15:00 +0100

    fixed resources page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -11,18 +11,18 @@ const page = () => {
 
             <div className="max-w-5xl flex items-center justify-center flex-col ">
 
-                <div className="w-full  text-sm items-center mb-6 justify-center flex">Framer templates and design tools
+                <div className="w-full px-8  text-sm items-center mb-6 justify-center flex">Framer templates and design tools
                 </div>
                 <div className="w-full recoleta text-5xl items-center justify-center font-light flex">Resources
 
                 </div>
 
-                <div>
+                <div className="w-full"  >
                     <TemplatesGrid />
                 </div>
 
 
-                <div className='w-full justify-center px-8  lg:px-0 items-center flex flex-col ' >
+                <div className='w-full justify-center px-8  lg:px-8 items-center flex flex-col ' >
                     <h2 className="text-3xl lg:text-2xl text-black text-opacity-75  font-semibold mb-8">Tools</h2>
 
                     <ToolsSection />
